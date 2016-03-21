16e1b2eb6a50bf37a0fc5b9384ee94340ed684b3
Kristoffer <clearsky1002@outlook.com>
2016-03-21 18:32:42 +0800

    Added Android packages

---
+++ Droid/Bootstrap/UserDialogsPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace ThisRoofN.Droid.Bootstrap
+{
+    public class UserDialogsPluginBootstrap
+        : MvxPluginBootstrapAction<Acr.MvvmCross.Plugins.UserDialogs.PluginLoader>
+    {
+    }
+}
